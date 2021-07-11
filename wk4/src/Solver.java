1207ddd87443119ccc0a523b13e6c150abc49f96
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2021-07-11 12:34:10 +0100

    first implementation of SearchNode inner class

---
+++ wk4/src/Solver.java
@@ -0,0 +1,65 @@
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+
+public class Solver {
+
+    private class SearchNode implements Comparable<SearchNode> {
+
+        public final Comparator<SearchNode> BY_MANHATTAN = new byManhattan();
+        private final Board board;
+        private final int moves;
+        private final SearchNode prevNode;
+
+        SearchNode(Board board, int moves, SearchNode prevNode) {
+            this.board = board;
+            this.moves = moves;
+            this.prevNode = prevNode;
+        }
+
+        public int compareTo(SearchNode that) {
+            return (this.board.hamming() + this.moves) - (that.board.hamming() + that.moves);
+        }
+
+        private class byManhattan implements Comparator<SearchNode> {
+
+            public int compare(SearchNode sn1, SearchNode sn2) {
+                return (sn1.board.manhattan() + sn1.moves) - (sn2.board.manhattan() + sn2.moves);
+            }
+
+        }
+
+
+
+    }
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        if (initial == null) {
+            throw new IllegalArgumentException();
+        }
+
+
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return false;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        return -1;
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+    }
+
+}
