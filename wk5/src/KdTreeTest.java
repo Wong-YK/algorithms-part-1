fdfc8ba5ef8a1a7393d58f5b3349cd3f3298be2d
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2021-07-25 15:38:52 +0100

    unit tests for size() method

--- wk5/src/KdTreeTest.java
+++ wk5/src/KdTreeTest.java
@@ -5,10 +5,10 @@ import edu.princeton.cs.algs4.Point2D;
 public class KdTreeTest {
 
     @Test
-    // The root of an empty KdTree is null
+    // KdTree initialised as empty
     public void KdTreeTest1() {
         KdTree kdt = new KdTree();
-        assertEquals(null, kdt.root);
+        assertTrue(kdt.isEmpty());
     }
 
     @Test
@@ -34,6 +34,51 @@ public class KdTreeTest {
     }
 
     @Test
+    // BST of size 5
+    public void sizeTest2() {
+        KdTree kdt = new KdTree();
+        Point2D p1 = new Point2D(0.5, 0.5);
+        Point2D p2 = new Point2D(0.7, 0.7);
+        Point2D p3 = new Point2D(0.8, 0.3);
+        Point2D p4 = new Point2D(0.65, 0.2);
+        Point2D p5 = new Point2D(0.3, 0.1);
+        kdt.insert(p1);
+        kdt.insert(p2);
+        kdt.insert(p3);
+        kdt.insert(p4);
+        kdt.insert(p5);
+        assertEquals(5, kdt.size());
+    }
+
+    @Test
+    // BST of size 10
+    public void sizeTest3() {
+        KdTree kdt = new KdTree();
+        Point2D p1 = new Point2D(0.5, 0.5);
+        Point2D p2 = new Point2D(0.7, 0.7);
+        Point2D p3 = new Point2D(0.8, 0.3);
+        Point2D p4 = new Point2D(0.65, 0.2);
+        Point2D p5 = new Point2D(0.3, 0.7);
+        Point2D p6 = new Point2D(0.2, 0.2);
+        Point2D p7 = new Point2D(0.1, 0.5);
+        Point2D p8 = new Point2D(0.1, 0.9);
+        Point2D p9 = new Point2D(0.7, 0.6);
+        Point2D p10 = new Point2D(0.95, 0.2);
+        kdt.insert(p1);
+        kdt.insert(p2);
+        kdt.insert(p3);
+        kdt.insert(p4);
+        kdt.insert(p5);
+        kdt.insert(p6);
+        kdt.insert(p7);
+        kdt.insert(p8);
+        kdt.insert(p9);
+        kdt.insert(p10);
+        assertEquals(10, kdt.size());
+    }
+
+
+    @Test
     // Insert single Point2D into BST
     public void insertTest1() {
         KdTree kdt = new KdTree();
@@ -51,11 +96,8 @@ public class KdTreeTest {
         Point2D p3 = new Point2D(0.8, 0.3);
         Point2D p4 = new Point2D(0.65, 0.2);
         kdt.insert(p1);
-        kdt.draw();
         kdt.insert(p2);
-        kdt.draw();
         kdt.insert(p3);
-        kdt.draw();
         kdt.insert(p4);
         kdt.draw();
     }
